a0931bb6271f64cf419c2f7136edc3d176a90f91
gustavocarvalhocompass <gustavo.carvalho@compasso.com.br>
2023-01-24 16:51:11 -0300

    creating supplier model

---
+++ MinimalAPI_DotNet6/Model/Supplier.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalAPI_DotNet6.Model
+{
+    public class Supplier
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? DocumentationNumber { get; set; }
+        public bool Active { get; set; }
+    }
+}
