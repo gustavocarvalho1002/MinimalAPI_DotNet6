930b28606ff2295160165f962e5a59507d8db7fd
gustavocarvalhocompass <gustavo.carvalho@compasso.com.br>
2023-01-24 18:24:37 -0300

    Seeting up EF and database connection

---
+++ MinimalAPI_DotNet6/Data/MinimalContextDb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPI_DotNet6.Model;
+
+namespace MinimalAPI_DotNet6.Data
+{
+    public class MinimalContextDb : DbContext
+    {
+        public MinimalContextDb(DbContextOptions<MinimalContextDb> options) : base(options) { }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Supplier>()
+                .HasKey(e => e.Id);
+
+            modelBuilder.Entity<Supplier>()
+                .Property(e => e.Name)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            modelBuilder.Entity<Supplier>()
+               .Property(e => e.DocumentationNumber)
+               .IsRequired()
+               .HasColumnType("varchar(14)");
+
+            modelBuilder.Entity<Supplier>()
+               .ToTable("Suppliers");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MinimalAPI_DotNet6/Migrations/20230124205533_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinimalAPIDotNet6.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Suppliers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "varchar(200)", nullable: false),
+                    DocumentationNumber = table.Column<string>(type: "varchar(14)", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suppliers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Suppliers");
+        }
+    }
+}
